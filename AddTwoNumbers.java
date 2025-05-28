f80344bd6b6be04e3fc19887283a6b337f27e3bb
Rishav gupta <rishavjais456@gmail.com>
2025-05-28 15:40:11 +0530

    Update AddTwoNumbers.java

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -1,8 +1,7 @@
 public class AddTwoNumbers {
     public static void main(String[] args) {
-        int a = 10; // First number
-        int b = 20; // Second number
-
+        int a = 10;
+        int b = 20;
         int sum = a + b;
         System.out.println("Sum of " + a + " and " + b + " is: " + sum);
     }
