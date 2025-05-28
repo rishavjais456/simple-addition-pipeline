c84c71109366334b18da2c50dda624dc32aed492
Rishav gupta <rishavjais456@gmail.com>
2025-05-28 15:37:16 +0530

    Update and rename add.py to AddTwoNumbers.java

---
+++ AddTwoNumbers.java
@@ -0,0 +1,9 @@
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+        int a = 10; // First number
+        int b = 20; // Second number
+
+        int sum = a + b;
+        System.out.println("Sum of " + a + " and " + b + " is: " + sum);
+    }
+}
--- add.py
+++
@@ -1,3 +0,0 @@
-a = 10
-b = 20
-print("The sum is:", a + b)
